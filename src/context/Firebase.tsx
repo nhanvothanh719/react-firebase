07c2b264cf8f8b9ef626d72deaa90b5239ebd59b
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-12-25 17:01:43 +0700

    Setup Firebase

---
+++ src/context/Firebase.tsx
@@ -0,0 +1,17 @@
+import { initializeApp, type FirebaseOptions } from 'firebase/app'
+import { createContext } from 'react'
+
+type FirebaseContextType = {}
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+export const FirebaseContext = createContext<FirebaseContextType | undefined>(undefined)
